a7a6a1ee284d6d82aaff77c623930f195bb60ee9
ElhamNawabi <nawabi.elham@gmail.com>
2023-09-13 19:21:34 -0400

    added more components and lorem ipsum placeholders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Intro from "./components/intro";
+import About from "./components/about";
+import Experience from './components/experience';
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <About />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const experience = () => {
+  return (
+    <div className="exp-container">
+        <h1 className="exp-header">Experience</h1>
+    </div>
+  )
+}
+
+export default experience
