e137ce95c2a452565d7af2f4119ae5bd41776e6b
ElhamNawabi <nawabi.elham@gmail.com>
2023-09-13 17:38:17 -0400

    spaced out social media icons

--- src/components/intro.js
+++ src/components/intro.js
@@ -12,11 +12,15 @@ const intro = () => {
             Hi, my name is Elham Nawabi and I'm a software developer
             with a passion for crafting seamless digital experiences!
         </h1>
-        <div className="icons">
-        <Button variant="dark" size="lg" href="https://drive.google.com/file/d/1uPhpdez7s9fkOLC-UThC-79c12uj2_qa/view?usp=sharing">Resume</Button>
-        <SocialIcon className="custom-linked" network="linkedin" bgColor="#212529" href="https://www.linkedin.com/in/elham-nawabi-bb2171164/" />
-        <SocialIcon className="custom-hub" network="github" href="https://github.com/ElhamNawabi" />
-        </div>
+        <span className="resume">
+          <Button variant="dark" size="lg" href="https://drive.google.com/file/d/1uPhpdez7s9fkOLC-UThC-79c12uj2_qa/view?usp=sharing">Resume</Button>
+        </span>
+        <span className="linkedin">
+          <SocialIcon className="custom-linked" network="linkedin" bgColor="#212529" href="https://www.linkedin.com/in/elham-nawabi-bb2171164/" />
+        </span>
+        <span className="github">
+          <SocialIcon className="custom-hub" network="github" href="https://github.com/ElhamNawabi" />
+        </span>
     </div>
   )
 }
